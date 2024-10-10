a633ea38367a23579b666a1dcad67a19c9c13c22
german nunez <german90nunez@gmail.com>
2024-10-10 13:47:42 -0700

    mobile resp card comp and blog page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,63 @@
 "use client"
 
 import React, { useState, useEffect } from 'react'
+import Card from './components/Card/Card'
+
+
+// Dunmmy post 
+const posts =[
+    {
+    id: 1,
+    title: 'Boost your conversion rate',
+    href: '#',
+    description:
+      'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
+    date: 'Mar 16, 2020',
+    datetime: '2020-03-16',
+    category: { title: 'Marketing', href: '#' },
+    author: {
+      name: 'Michael Foster',
+      role: 'Co-Founder / CTO',
+      href: '#',
+      imageUrl:
+        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+  },
+    {
+    id: 1,
+    title: 'Boost your conversion rate',
+    href: '#',
+    description:
+      'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
+    date: 'Mar 16, 2020',
+    datetime: '2020-03-16',
+    category: { title: 'Marketing', href: '#' },
+    author: {
+      name: 'Michael Foster',
+      role: 'Co-Founder / CTO',
+      href: '#',
+      imageUrl:
+        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+  },
+    {
+    id: 1,
+    title: 'Boost your conversion rate',
+    href: '#',
+    description:
+      'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
+    date: 'Mar 16, 2020',
+    datetime: '2020-03-16',
+    category: { title: 'Marketing', href: '#' },
+    // author: {
+    //   name: 'Michael Foster',
+    //   role: 'Co-Founder / CTO',
+    //   href: '#',
+    //   imageUrl:
+    //     'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    // },
+  }
+]
 
 export default function Home() {
   const [ activeTheme, setActiveTheme ] = useState<string>("theme")
@@ -58,6 +115,7 @@ export default function Home() {
   return (
     <main className="">
       <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+       {/* Hero */}
         <div className="text-center">
           <h1 className="text-balance text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
             Your source for all your furr-friends trends
@@ -76,7 +134,25 @@ export default function Home() {
             </a>
           </div>
         </div>
-      </div>
+
+        {/* Blogs */}
+
+        <div className="mx-auto max-w-2xl lg:mx-0">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">From the blog</h2>
+          <p className="mt-2 text-lg leading-8 text-gray-600">
+            Learn how to grow your business with our expert advice.
+          </p>
+        </div>
+        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          {
+            posts.map((post: any, index: number) => {
+             return  <Card 
+                      key={index}
+                      post={post}/>
+            })
+          }
+        </div>
+      </div>      
     </main>
   );
 }
